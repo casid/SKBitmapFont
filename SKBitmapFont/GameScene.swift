e1a24bd4a1ada3a52df9e713566242a7c6d75ce7
casid <andy@mazebert.com>
2015-10-01 23:51:21 +0200

    Added vertical alignment.

--- SKBitmapFont/GameScene.swift
+++ SKBitmapFont/GameScene.swift
@@ -12,24 +12,35 @@ class GameScene: SKScene {
     let font = BitmapFont(name: "art.scnassets/font")
     
     override func didMoveToView(view: SKView) {
+        //testHorizontalAlignment()
+        testVerticalAlignment()
+    }
+    
+    private func testHorizontalAlignment() {
         addLeftAlignedLabel()
         addRightAlignedLabel()
         addCenterAlignedLabel()
     }
     
+    private func testVerticalAlignment() {
+        addTopAlignedLabel()
+        addVCenterAlignedLabel()
+        addBottomAlignedLabel()
+    }
+    
     private func addLeftAlignedLabel() {
         let label = BitmapLabel(font: font)
         label.text = "abcC-0913 kf!=?.;_\nHello, Sir!"
-        label.position.y = self.size.height
         label.position.x = 0
+        label.position.y = self.size.height
         self.addChild(label)
     }
     
     private func addRightAlignedLabel() {
         let label = BitmapLabel(font: font)
         label.text = "abcC-0913 kf!=?.;_\nHello, Sir!"
-        label.position.y = self.size.height - 200
         label.position.x = self.size.width
+        label.position.y = self.size.height - 200
         label.horizontalAlignment = BitmapLabelHorizontalAlignment.Right
         self.addChild(label)
     }
@@ -37,9 +48,36 @@ class GameScene: SKScene {
     private func addCenterAlignedLabel() {
         let label = BitmapLabel(font: font)
         label.text = "abcC-0913 kf!=?.;_\nHello, Sir!"
-        label.position.y = self.size.height - 400
         label.position.x = 0.5 * self.size.width
+        label.position.y = self.size.height - 400
         label.horizontalAlignment = BitmapLabelHorizontalAlignment.Center
+        self.addChild(label)
+    }
+    
+    private func addTopAlignedLabel() {
+        let label = BitmapLabel(font: font)
+        label.text = "Top"
+        label.position.x = 0
+        label.position.y = self.size.height
+        self.addChild(label)
+    }
+    
+    private func addVCenterAlignedLabel() {
+        let label = BitmapLabel(font: font)
+        label.text = "a\n\n\ncenter\naa\naaa\naaaa"
+        label.position.x = 0.5 * self.size.width
+        label.position.y = 0.5 * self.size.height
+        label.horizontalAlignment = BitmapLabelHorizontalAlignment.Center
+        label.verticalAlignment = BitmapLabelVerticalAlignment.Center
+        self.addChild(label)
+    }
+    
+    private func addBottomAlignedLabel() {
+        let label = BitmapLabel(font: font)
+        label.text = "Bottom"
+        label.position.x = 0
+        label.position.y = 0
+        label.verticalAlignment = BitmapLabelVerticalAlignment.Bottom
         self.addChild(label)
     }
    
--- SKBitmapFont/src/BitmapLabel.swift
+++ SKBitmapFont/src/BitmapLabel.swift
@@ -6,6 +6,12 @@ public enum BitmapLabelHorizontalAlignment:Int {
     case Right
 }
 
+public enum BitmapLabelVerticalAlignment:Int {
+    case Top
+    case Center
+    case Bottom
+}
+
 public class BitmapLabel : SKNode, BitmapFontLoadObserver {
     private let font:BitmapFont
     private var letters = [SKSpriteNode]()
@@ -20,11 +26,17 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
             layout()
         }
     }
+    public var verticalAlignment:BitmapLabelVerticalAlignment {
+        didSet {
+            layout()
+        }
+    }
     
     public init(font:BitmapFont) {
         self.font = font
         self.text = ""
         self.horizontalAlignment = BitmapLabelHorizontalAlignment.Left
+        self.verticalAlignment = BitmapLabelVerticalAlignment.Top
         
         super.init()
         
@@ -47,14 +59,15 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
             var y:CGFloat = 0
             var lineIndex:Int = 0
             var index:Int = 0
+            let anchorPointY:CGFloat = getLetterAnchorPointY()
             
             for character in text.characters {
                 if let char = font.getChar(character) {
                     let sprite = SKSpriteNode(texture: char.texture)
                     sprite.anchorPoint.x = 0
-                    sprite.anchorPoint.y = 1
+                    sprite.anchorPoint.y = anchorPointY
                     sprite.position.x = x + CGFloat(char.xOffset)
-                    sprite.position.y = y - CGFloat(char.yOffset)
+                    sprite.position.y = y - anchorPointY * CGFloat(char.yOffset)
                     addChild(sprite)
                     letters.append(sprite)
                     
@@ -74,7 +87,19 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
             if x != 0 {
                 applyHorizontalAlignment(lineIndex, toIndex: index - 1, lineWidth: x)
             }
+            
+            applyVerticalAlignment()
         }
+    }
+    
+    private func getLetterAnchorPointY() -> CGFloat {
+        if verticalAlignment == BitmapLabelVerticalAlignment.Bottom {
+            return 0
+        } else if verticalAlignment == BitmapLabelVerticalAlignment.Center {
+            return 0.5
+        }
+        
+        return 1
     }
     
     private func applyHorizontalAlignment(fromIndex:Int, toIndex:Int, lineWidth:CGFloat) {
@@ -88,6 +113,34 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
         if offset != 0 {
             for var index = fromIndex; index <= toIndex; ++index {
                 letters[index].position.x += offset
+            }
+        }
+    }
+    
+    private func applyVerticalAlignment() {
+        if verticalAlignment == BitmapLabelVerticalAlignment.Top {
+            return
+        }
+        
+        var offset:CGFloat = 0
+        var minY:CGFloat = CGFloat.max
+        var maxY:CGFloat = CGFloat.min
+        
+        for letter in letters {
+            minY = min(letter.position.y - (1 - letter.anchorPoint.y) * letter.size.height, minY)
+            maxY = max(letter.position.y + letter.anchorPoint.y * letter.size.height, maxY)
+        }
+        
+        let delta = maxY - minY
+        if verticalAlignment == BitmapLabelVerticalAlignment.Bottom {
+            offset = delta
+        } else if verticalAlignment == BitmapLabelVerticalAlignment.Center {
+            offset = 0.5 * delta
+        }
+        
+        if offset != 0 {
+            for letter in letters {
+                letter.position.y += offset
             }
         }
     }
