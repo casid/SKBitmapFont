fbf1bb158bd47155ca09545494e7a56f20f1aacd
casid <andy@mazebert.com>
2015-10-01 22:47:13 +0200

    Added horizontal alignment.

--- SKBitmapFont/GameScene.swift
+++ SKBitmapFont/GameScene.swift
@@ -9,13 +9,37 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let font = BitmapFont(name: "art.scnassets/font")
+    
     override func didMoveToView(view: SKView) {
-        let font = BitmapFont(name: "art.scnassets/font")
+        addLeftAlignedLabel()
+        addRightAlignedLabel()
+        addCenterAlignedLabel()
+    }
+    
+    private func addLeftAlignedLabel() {
         let label = BitmapLabel(font: font)
         label.text = "abcC-0913 kf!=?.;_\nHello, Sir!"
-        label.position.y = 300
-        label.position.x = 300
-        
+        label.position.y = self.size.height
+        label.position.x = 0
+        self.addChild(label)
+    }
+    
+    private func addRightAlignedLabel() {
+        let label = BitmapLabel(font: font)
+        label.text = "abcC-0913 kf!=?.;_\nHello, Sir!"
+        label.position.y = self.size.height - 200
+        label.position.x = self.size.width
+        label.horizontalAlignment = BitmapLabelHorizontalAlignment.Right
+        self.addChild(label)
+    }
+    
+    private func addCenterAlignedLabel() {
+        let label = BitmapLabel(font: font)
+        label.text = "abcC-0913 kf!=?.;_\nHello, Sir!"
+        label.position.y = self.size.height - 400
+        label.position.x = 0.5 * self.size.width
+        label.horizontalAlignment = BitmapLabelHorizontalAlignment.Center
         self.addChild(label)
     }
    
--- SKBitmapFont/src/BitmapLabel.swift
+++ SKBitmapFont/src/BitmapLabel.swift
@@ -1,9 +1,22 @@
 import SpriteKit
 
+public enum BitmapLabelHorizontalAlignment:Int {
+    case Left
+    case Center
+    case Right
+}
+
 public class BitmapLabel : SKNode, BitmapFontLoadObserver {
     private let font:BitmapFont
+    private var letters = [SKSpriteNode]()
+    
     public var text:String {
         didSet(text) {
+            layout()
+        }
+    }
+    public var horizontalAlignment:BitmapLabelHorizontalAlignment {
+        didSet {
             layout()
         }
     }
@@ -11,6 +24,7 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
     public init(font:BitmapFont) {
         self.font = font
         self.text = ""
+        self.horizontalAlignment = BitmapLabelHorizontalAlignment.Left
         
         super.init()
         
@@ -25,11 +39,14 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
     
     private func layout() {
         self.removeAllChildren()
+        letters.removeAll()
         
         if font.isLoaded {
             
             var x:CGFloat = 0
             var y:CGFloat = 0
+            var lineIndex:Int = 0
+            var index:Int = 0
             
             for character in text.characters {
                 if let char = font.getChar(character) {
@@ -39,14 +56,38 @@ public class BitmapLabel : SKNode, BitmapFontLoadObserver {
                     sprite.position.x = x + CGFloat(char.xOffset)
                     sprite.position.y = y - CGFloat(char.yOffset)
                     addChild(sprite)
+                    letters.append(sprite)
                     
                     x += CGFloat(char.xAdvance)
+                    ++index
                 } else {
                     if character == "\n" {
+                        applyHorizontalAlignment(lineIndex, toIndex: index - 1, lineWidth: x)
+                        
                         x = 0
                         y -= CGFloat(font.lineHeight)
+                        lineIndex = index
                     }
                 }
+            }
+            
+            if x != 0 {
+                applyHorizontalAlignment(lineIndex, toIndex: index - 1, lineWidth: x)
+            }
+        }
+    }
+    
+    private func applyHorizontalAlignment(fromIndex:Int, toIndex:Int, lineWidth:CGFloat) {
+        var offset:CGFloat = 0
+        if horizontalAlignment == BitmapLabelHorizontalAlignment.Right {
+            offset = -lineWidth
+        } else if horizontalAlignment == BitmapLabelHorizontalAlignment.Center {
+            offset = -0.5 * lineWidth
+        }
+        
+        if offset != 0 {
+            for var index = fromIndex; index <= toIndex; ++index {
+                letters[index].position.x += offset
             }
         }
     }
